269cca7af56301afc8cced6b30bd6a26486cb55e
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-19 16:08:03 -0600

    alter authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   protect_from_forgery
-  skip_before_action :verify_authenticity_token, if: :json_request?
+  # skip_before_action :verify_authenticity_token, if: :json_request?
+  skip_before_filter :verify_authenticity_token
 
   protected
 
