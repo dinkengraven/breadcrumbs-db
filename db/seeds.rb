760a61a601c7a4a8ecbe95bb665c4810b0435c56
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-19 11:14:21 -0600

    add seeds from sinatra app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create([
+  {first_name: "Katelyn", last_name: "D", email: "katelyn@email.com", password: "password"},
+  {first_name: "Ben", last_name: "F", email: "salty@email.com", password: "password"},
+  {first_name: "Jen", last_name: "T", email: "jtru@email.com", password: "password"}
+])
