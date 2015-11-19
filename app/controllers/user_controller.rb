234ddf7c8b4492aa12948f9c392ffc3a195d330d
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-19 10:59:53 -0600

    start filling in user controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,17 @@
+class UserController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      content_type :json
+      return { user: {id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email} }.to_json
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+end
