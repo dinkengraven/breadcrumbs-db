b1604666441763483b2cce098511ef8c30a0d9a2
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-20 11:12:04 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119213020) do
+ActiveRecord::Schema.define(version: 20151120170716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20151119213020) do
     t.integer  "receiver_id"
     t.string   "geolocation"
     t.text     "body"
-    t.string   "path_to_photo"
     t.boolean  "found"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
@@ -40,6 +39,15 @@ ActiveRecord::Schema.define(version: 20151119213020) do
 
   add_index "friendships", ["friend_id"], name: "index_friendships_on_friend_id", using: :btree
   add_index "friendships", ["user_id"], name: "index_friendships_on_user_id", using: :btree
+
+  create_table "pseudocrumbs", force: :cascade do |t|
+    t.decimal  "lat"
+    t.decimal  "long"
+    t.string   "identifier"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      null: false
