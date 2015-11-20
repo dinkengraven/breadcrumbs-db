15ace80c67a3fad0e2af16112c1e54c738d2c639
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-20 12:17:19 -0600

    add routes for pseudocrumbs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :breadcrumbs
+  resources :pseudocrumbs
 
   post "/login" => "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
