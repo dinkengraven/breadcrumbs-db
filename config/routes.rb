d18b3d425f1a6e6df3cfae8be8f784bec5f638b8
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-21 15:57:49 -0600

    add route for fetching all breadcrumbs

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   post "/login" => "sessions#create"
   get "/retrieve" => "pseudocrumbs#return_all_for_user"
+  get "/fetch" => "breadcrumbs#return_all_for_user"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
