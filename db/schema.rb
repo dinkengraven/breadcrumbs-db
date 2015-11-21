ca72b864c87d7ecfe945d074a0b848034d5bac29
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-21 12:50:49 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120183138) do
+ActiveRecord::Schema.define(version: 20151121184429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,12 @@ ActiveRecord::Schema.define(version: 20151120183138) do
     t.float    "lat"
     t.float    "long"
     t.string   "identifier"
-    t.text     "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "title"
+    t.string   "subtitle"
+    t.integer  "creator_id"
+    t.integer  "receiver_id"
   end
 
   create_table "users", force: :cascade do |t|
