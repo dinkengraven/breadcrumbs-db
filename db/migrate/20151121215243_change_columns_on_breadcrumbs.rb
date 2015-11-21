70fe422e4d72faa7221ea1c5a157b046039a7769
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-21 15:58:23 -0600

    change columns on breadcrumbs table

---
+++ db/migrate/20151121215243_change_columns_on_breadcrumbs.rb
@@ -0,0 +1,10 @@
+class ChangeColumnsOnBreadcrumbs < ActiveRecord::Migration
+  def change
+    remove_column :breadcrumbs, :geolocation
+    remove_column :breadcrumbs, :body
+    add_column :breadcrumbs, :lat, :float
+    add_column :breadcrumbs, :long, :float
+    add_column :breadcrumbs, :title, :string
+    add_column :breadcrumbs, :title, :string
+  end
+end
