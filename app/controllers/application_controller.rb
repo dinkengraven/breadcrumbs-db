b3f4f1ba5eb38851aa760ea594d0da2812e002d3
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-22 13:02:49 -0600

    switch back to auth tokens to test api key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  protect_from_forgery
   # skip_before_action :verify_authenticity_token, if: :json_request?
-  skip_before_filter :verify_authenticity_token
+
+  protect_from_forgery
+  # skip_before_filter :verify_authenticity_token
 
   protected
 
