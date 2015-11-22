885c5e822f826129bb767b1850e406c4b5e55893
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-22 13:21:33 -0600

    switch #restrict_access to expect token instead of params

--- app/controllers/breadcrumbs_controller.rb
+++ app/controllers/breadcrumbs_controller.rb
@@ -43,7 +43,10 @@ class BreadcrumbsController < ApplicationController
     end
 
     def restrict_access
-      api_key = ApiKey.find_by(access_token: params[:access_token])
-      head :unauthorized unless api_key
+      # api_key = ApiKey.find_by(access_token: params[:access_token])
+      # head :unauthorized unless api_key
+      authenticate_or_request_with_http_token do |token, options|
+        ApiKey.exists?(access_token: token)
+      end
     end
 end
