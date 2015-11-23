cb7c3254f12f21105cd18c28a58b991188274b1c
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-23 14:36:36 -0600

    refactor 200 OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: {
-        status: 200,
+      render status: 200, json: {
         message: "Successfully registered."
       }.to_json
     end
