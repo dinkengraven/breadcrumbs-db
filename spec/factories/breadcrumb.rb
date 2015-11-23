cce20bdb20d909ef56d0817d637a87429fcc8263
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-23 13:53:17 -0600

    try adding photo (so far unsuccessful)

--- spec/factories/breadcrumb.rb
+++ spec/factories/breadcrumb.rb
@@ -5,6 +5,9 @@ FactoryGirl.define do
     title "You've got a breadCrumb!"
     subtitle "hello seattle"
     identifier "44C8D038-7815-45D3-B3D2-40E1A6191C3B"
+    photo File.new(Rails.root + 'spec/factories/images/rails.png') 
+    # photo { fixture_file_upload "#{Rails.root}/spec/fixtures/images/product.png", 'image/png' }
+
   end
 
 end
